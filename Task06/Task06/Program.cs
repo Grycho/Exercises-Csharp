187c7fe5e2b91669bd3e6abda945e6701d7cbf31
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 22:19:35 +0200

    description added

--- Task06/Task06/Program.cs
+++ Task06/Task06/Program.cs
@@ -26,13 +26,13 @@ namespace Task06
                 int result = ((a - b) * b);
                 Console.WriteLine("\nThe value of the expression is: {0}", result);
             }
-            else if ((a - b) == 0)
+            else if ((a - b) == 0)                                                  // [after test] I added another "else if" for denominator if would be zero(for statement)
             {
                 Console.WriteLine("\nWe don't divide by ZERO!");
             }
             else
             {
-                double result = (double)1 / (a - b);                                // on else must be changed for double variable because here is denominator
+                double result = (double)1 / (a - b);                                // on else I changed for double variable because here is denominator
                 Console.WriteLine("\nThe value of the expression is: {0}", result);
             }
         }
