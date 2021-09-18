4f1a4f915c4b4d4ae86e6ee1f95188309ac888fe
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 19:43:06 +0200

    declared variables

--- Task03/Task03/Program.cs
+++ Task03/Task03/Program.cs
@@ -8,6 +8,14 @@ namespace Task03
         {
             // Calculate the area of the triangle from the length of the 3 sides
 
+            double a, b, c;
+            Console.WriteLine("Enter the length of the side a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of the side b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of the side c: ");
+            c = double.Parse(Console.ReadLine());
+
 
         }
     }
