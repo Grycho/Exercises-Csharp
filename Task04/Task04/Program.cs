89703fe56e980ddffca2afbedcf7b6492ba76d42
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 21:07:16 +0200

    adding power to expression and added else

--- Task04/Task04/Program.cs
+++ Task04/Task04/Program.cs
@@ -14,9 +14,14 @@ namespace Task04
             Console.WriteLine("Enter a value B: ");
             b = double.Parse(Console.ReadLine());
 
-            if ((a + b)*(a + b) == 0)
+            if ((a + b)*(a + b) != 0)
             {
-
+                double expression;
+                expression = (a * a + b) / Math.Pow(a + b, 2);  // adding a power to an expression (a2 + b) / (a + b)2
+            }
+            else
+            {
+                Console.WriteLine("We don't divide by ZERO");   // if the denominator is zero show this announcement
             }
         }
     }
