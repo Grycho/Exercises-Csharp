8ca6399e1a2b58eb767a65bdddeb6fc6220e74bb
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 21:21:51 +0200

    if loop added

--- Task05/Task05/Program.cs
+++ Task05/Task05/Program.cs
@@ -10,6 +10,20 @@ namespace Task05
 
             int temp;
             Console.WriteLine("Enter the water temperature: ");
+            temp = int.Parse(Console.ReadLine());
+
+            if (temp > 100)
+            {
+                Console.WriteLine("STEAM");
+            }
+            else if (temp < 0)
+            {
+                Console.WriteLine("ICE");
+            }
+            else
+            {
+                Console.WriteLine("LIQUID");
+            }
         }
     }
 }
