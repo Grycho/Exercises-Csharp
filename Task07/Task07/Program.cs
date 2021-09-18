24121369cb5609e37875ba4aea1e10fb709a1d8a
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 22:22:02 +0200

    description added

---
+++ Task07/Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read in a rating from 2 to 5 and display it in words
+
+            
+        }
+    }
+}
