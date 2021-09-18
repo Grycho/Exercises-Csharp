1c420c199480e0c0f296244d733d3425ed6facc6
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 20:53:52 +0200

    description added and variables

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // evaluating the value of an expression
+
+            double a, b;
+            Console.WriteLine("Enter a value A: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value B: ");
+            b = double.Parse(Console.ReadLine());
+        }
+    }
+}
