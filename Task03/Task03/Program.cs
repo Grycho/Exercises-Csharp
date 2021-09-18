54f7c10eca29741025a2ddd3c36733bbec856e1a
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 19:54:26 +0200

    adding a result display

--- Task03/Task03/Program.cs
+++ Task03/Task03/Program.cs
@@ -9,15 +9,16 @@ namespace Task03
             // Calculate the area of the triangle from the length of the 3 sides
 
             double a, b, c;
-            Console.WriteLine("Enter the length of the side a: ");
+            Console.Write("Enter the length of the side a: ");
             a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the length of the side b: ");
+            Console.Write("Enter the length of the side b: ");
             b = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the length of the side c: ");
+            Console.Write("Enter the length of the side c: ");
             c = double.Parse(Console.ReadLine());
 
-            double area, p = (a + b + c) / 2;
-            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            double area, p = (a + b + c) / 2;                               //expression added to the variable
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));              //an expression that uses a square root
+            Console.WriteLine("The area of the triangle is = {0}", area);
         }
     }
 }
