2951f88183c6d33a146a1b30247e627cd7b272b8
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 21:29:57 +0200

    added some descriptions

--- Task05/Task05/Program.cs
+++ Task05/Task05/Program.cs
@@ -9,21 +9,22 @@ namespace Task05
             //Read the water temperature and view its status
 
             int temp;
-            Console.WriteLine("Enter the water temperature: ");
+            Console.Write("Enter the water temperature: ");
             temp = int.Parse(Console.ReadLine());
 
-            if (temp > 100)
+            if (temp > 100)                     // if the entered temperature is above 100 degrees Celsius show this is Steam
             {
                 Console.WriteLine("STEAM");
             }
-            else if (temp < 0)
+            else if (temp < 0)                  // if the entered temperature is less than 0 degrees Celsius show this is Ice
             {
                 Console.WriteLine("ICE");
             }
-            else
+            else                                // if the entered temperature is not above 100 and not less than 0 degrees Celsius show this is Liquid
             {
                 Console.WriteLine("LIQUID");
             }
+            Console.WriteLine("\nEnd of test! Push Enter for leave...");
         }
     }
 }
