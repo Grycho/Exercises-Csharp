c1abff39d203a6a48257bd10715b30fb61a9272e
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 19:39:41 +0200

    description added to the new programm

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculate the area of the triangle from the length of the 3 sides
+
+
+        }
+    }
+}
