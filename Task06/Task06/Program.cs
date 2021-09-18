cefb2858daa6f0a860d32361a69e6f7e15c35861
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 21:42:08 +0200

    description added

---
+++ Task06/Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using variables of type double calculate the expression
+
+
+        }
+    }
+}
