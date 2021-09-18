328326e624e95585624f109c752f2669e3b77faa
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 21:48:47 +0200

    description changed, variables added

--- Task06/Task06/Program.cs
+++ Task06/Task06/Program.cs
@@ -6,7 +6,15 @@ namespace Task06
     {
         static void Main(string[] args)
         {
-            //Using variables of type double calculate the expression
+            //Using variables of type double calculate the expression: a2 + b for c>0; a - b2 for c<0; 1/a-b for c=0
+
+            int a, b, c;
+            Console.WriteLine("Enter a value for a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value for b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value for c: ");
+            c = int.Parse(Console.ReadLine());
 
 
         }
