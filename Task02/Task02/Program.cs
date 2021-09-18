8d2f1c25866c481ec6b7d338b9fa1e80e4952969
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-18 19:05:37 +0200

    declared variables

--- Task02/Task02/Program.cs
+++ Task02/Task02/Program.cs
@@ -7,6 +7,14 @@ namespace Task02
         static void Main(string[] args)
         {
             // Calculate the hypotenuse in a right triangle
+
+            int a, b;
+            Console.WriteLine("Enter the length: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width: ");
+            b = int.Parse(Console.ReadLine());
+
+            double c;
         }
     }
 }
