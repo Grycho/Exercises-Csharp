af898ccf311caacbc89e3015c874860ab71b01c7
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 18:36:56 +0200

    variables added

--- Task11a/Task11a/Program.cs
+++ Task11a/Task11a/Program.cs
@@ -7,8 +7,14 @@ namespace Task11a
         static void Main(string[] args)
         {
             //Build a triangle if there are dependencies at the same time: (𝑎<𝑏+𝑐),(𝑏<𝑎+𝑐),(𝑐<𝑎+𝑏)
-
-
+            int a, b, c;
+            bool numb = true;
+            Console.Write("Enter the length a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter the length b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Enter the length c: ");
+            c = int.Parse(Console.ReadLine());
         }
     }
 }
