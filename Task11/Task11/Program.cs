1df9151f56c6d63066ab84d2d30a8ca74643ee01
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 16:05:13 +0200

    description added

---
+++ Task11/Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Loads the lengths a, b, c of the three segments. 
+             * If any value is less than zero, the program displays an input error message. 
+             * Otherwise, the program displays one of the messages: "You can build a triangle out of these sections" 
+             * or "You can't build a triangle out of these sections".*/
+
+        }
+    }
+}
