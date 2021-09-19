b64382252d98ae0bde2eb42453df3542ee716200
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 16:11:37 +0200

    variables added

--- Task11/Task11/Program.cs
+++ Task11/Task11/Program.cs
@@ -10,7 +10,8 @@ namespace Task11
              * If any value is less than zero, the program displays an input error message. 
              * Otherwise, the program displays one of the messages: "You can build a triangle out of these sections" 
              * or "You can't build a triangle out of these sections".*/
-
+            int a, b, c;
+            bool correct = true;
         }
     }
 }
