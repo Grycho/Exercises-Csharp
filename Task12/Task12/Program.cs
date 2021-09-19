ebedfa8b66588f5ace5769fcb031feabc589c53e
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 18:55:55 +0200

    variables added

---
+++ Task12/Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The cost of printing one copy of the book is 10$ for a circulation of more than 1000, 
+             * 12$ for a circulation of 500 to 1000 and 15$ for a circulation of less than 500. 
+             * For the entered value - the number of books to be printed, it is to provide 
+             * the cost of printing the entire run.*/
+
+            int book;
+            Console.Write("How many books you want to print: ");
+            book = int.Parse(Console.ReadLine());
+            int whole;
+
+            
+        }
+    }
+}
