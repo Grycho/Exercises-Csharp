68e6ac2619d01587e853236da9f394598b298918
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 19:54:33 +0200

    variables and description added

---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program which a given numerical month will indicate how many days have elapsed
+             * from the beginning of the year to the 1st day of the given month*/
+
+            int month;
+            Console.Write("Enter the month(number) and we will se how many days elapsed: ");
+            month = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
