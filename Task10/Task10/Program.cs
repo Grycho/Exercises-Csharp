a371e1d43626d91cd578f2e0b483f2669138d63a
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 15:21:50 +0200

    variables added

--- Task10/Task10/Program.cs
+++ Task10/Task10/Program.cs
@@ -11,7 +11,11 @@ namespace Task10
              * he receives 30$ for each item sold, and if he sold less, he receives 20$ for each item.
              * The input data to the program should be the number of items sold in a given settlement period.*/
 
+            int pieces;
+            Console.Write("How many pieces have you sold: ");
+            pieces = int.Parse(Console.ReadLine());
 
+            int payment;
         }
     }
 }
