6658a5f5cf524268e2cd1a0ebfdda413e4bc185b
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 14:01:06 +0200

    description added

---
+++ Task08/Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A program that checks if a year is leap year
+
+
+        }
+    }
+}
