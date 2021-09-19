178574bfab28cfb2c19abc15f99af88b5393870c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 19:35:36 +0200

    description added

---
+++ Task13/Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The program reads the number of the day and displays name in words. Use the switch statement.
+
+
+        }
+    }
+}
