f102927edf502b4a2eac91b26c59a0abedea8d91
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 15:16:41 +0200

    description added

---
+++ Task10/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that calculates the salary of a seller of a certain product.
+             * If in a given settlement period the seller has sold more than 50 items of the product,
+             * he receives 30$ for each item sold, and if he sold less, he receives 20$ for each item.
+             * The input data to the program should be the number of items sold in a given settlement period.*/
+
+
+        }
+    }
+}
