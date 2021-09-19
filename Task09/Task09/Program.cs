84b51099b0d406b103649322163061cf1023dfa0
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 14:55:26 +0200

    description added

---
+++ Task09/Task09/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program checks if the loaded number is integer has the penultimate 
+             * digit greater than 4 and displays appropriate messages, if it is and it is not.*/
+
+
+        }
+    }
+}
