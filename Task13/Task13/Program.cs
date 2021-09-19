67c82ace6f264a689b70a76da5922b62c6179f82
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-19 19:42:38 +0200

    switch function added

--- Task13/Task13/Program.cs
+++ Task13/Task13/Program.cs
@@ -12,7 +12,17 @@ namespace Task13
             Console.WriteLine("Which day of the week: ");
             days = int.Parse(Console.ReadLine());
 
-            
+            switch (days)
+            {
+                case 1: Console.WriteLine("Monday"); break;
+                case 2: Console.WriteLine("Tuesday"); break;
+                case 3: Console.WriteLine("Wednesday"); break;
+                case 4: Console.WriteLine("Thursday"); break;
+                case 5: Console.WriteLine("Friday"); break;
+                case 6: Console.WriteLine("Saturday"); break;
+                case 7: Console.WriteLine("Sunday"); break;
+                default: Console.WriteLine("Wrong value given!"); break;
+            }
         }
     }
 }
