076f11dfcb9dbbfc4f53a6c5ab1aa6610373a589
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 23:17:12 +0200

    description changed

--- Task16/Task16/Program.cs
+++ Task16/Task16/Program.cs
@@ -6,7 +6,7 @@ namespace Task16
     {
         static void Main(string[] args)
         {
-            /* (Percentage calculation) A program that allows you to calculate the amount of the monthly installment 
+            /* [Percentage calculation] A program that allows you to calculate the amount of the monthly installment 
              * for the purchased equipment. The input data for the program are: 
              * the price of the goods (from 100$ to 10,000$) and the number of installments (from 6 to 48).*/
 
