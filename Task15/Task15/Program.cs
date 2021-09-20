0f3ddbf1c1c8a65556ea9ffe8fc8f83e6ee798bc
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 19:10:17 +0200

    variables added

--- Task15/Task15/Program.cs
+++ Task15/Task15/Program.cs
@@ -8,6 +8,12 @@ namespace Task15
         {
             //A menu program that will execute the following commands: total, difference, product, quotient, data entry, program exit.
 
+            double a, b;
+            Console.Write("Enter the FIRST number: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Enter the FIRST number: ");
+            b = double.Parse(Console.ReadLine());
+
 
         }
     }
