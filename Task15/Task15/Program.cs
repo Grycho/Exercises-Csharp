24bdd2afb66dcee3a4ab14cbbde68003aeb739d9
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 19:17:09 +0200

    switch function and more variables added

--- Task15/Task15/Program.cs
+++ Task15/Task15/Program.cs
@@ -13,8 +13,17 @@ namespace Task15
             a = double.Parse(Console.ReadLine());
             Console.Write("Enter the FIRST number: ");
             b = double.Parse(Console.ReadLine());
+            int menu;
+            menu = int.Parse(Console.ReadLine());
 
-
+            switch (menu)
+            {
+                case 1: Console.WriteLine("1. Addition"); break;
+                case 2: Console.WriteLine("2. Subtraction"); break;
+                case 3: Console.WriteLine("3. Multiplication"); break;
+                case 4: Console.WriteLine("4. Division"); break;
+                default: Console.WriteLine("Incorrect value entered!"); break;
+            }
         }
     }
 }
