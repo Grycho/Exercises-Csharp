319bb0ef9ef72f64c2ec6db503f2eb4205b4cc12
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 18:59:23 +0200

    description added

---
+++ Task15/Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A menu program that will execute the following commands: total, difference, product, quotient, data entry, program exit.
+
+
+        }
+    }
+}
