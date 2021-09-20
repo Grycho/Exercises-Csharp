c81b56be8d3dabc62e1aaa8d8386fe772ba06bf5
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 20:50:50 +0200

    variables added

--- Task16/Task16/Program.cs
+++ Task16/Task16/Program.cs
@@ -10,7 +10,12 @@ namespace Task16
              * for the purchased equipment. The input data for the program are: 
              * the price of the goods (from 100$ to 10,000$) and the number of installments (from 6 to 48).*/
 
-
+            int installment;
+            double priceGoods;
+            Console.Write("The price of the goods(from 100$ to 10,000$): ");
+            priceGoods = double.Parse(Console.ReadLine());
+            Console.Write("How many installments do you want to take out a credit for(from 6 to 48): ");
+            installment = int.Parse(Console.ReadLine());
         }
     }
 }
