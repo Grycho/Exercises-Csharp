80dab695692b179a9445865b46a45ece63fd3b8e
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-20 20:32:33 +0200

    description added

---
+++ Task16/Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that allows you to calculate the amount of the monthly installment 
+             * for the purchased equipment. The input data for the program are: 
+             * the price of the goods (from 100$ to 10,000$) and the number of installments (from 6 to 48).*/
+
+
+        }
+    }
+}
