d3098e6862a2c2c64625a417df781f03a877f925
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-21 23:22:19 +0200

    Display changed. Before show result=0 after "default" and "divide be 0".

--- Task17/Task17/Program.cs
+++ Task17/Task17/Program.cs
@@ -21,9 +21,9 @@ namespace Task17
 
             switch (sign)
             {
-                case "+": result = num1 + num2; break;
-                case "-": result = num1 - num2; break;
-                case "*": result = num1 * num2; break;
+                case "+": result = num1 + num2; Console.WriteLine("\nResult = " + result); break;
+                case "-": result = num1 - num2; Console.WriteLine("\nResult = " + result); break;
+                case "*": result = num1 * num2; Console.WriteLine("\nResult = " + result); break;
                 case "/": 
                     if (num2 == 0) 
                     { 
@@ -32,11 +32,11 @@ namespace Task17
                     else
                     {
                         result = num1 / num2;
+                        Console.WriteLine("\nResult = " + result);
                     }
                     break;
                 default: Console.WriteLine("\nOption out of range!"); break;
             }
-            Console.WriteLine("\nResult = " + result);
         }
     }
 }
