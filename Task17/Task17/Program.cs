7d30851bc0fcb9e5eccf3931fd4c23bc0bec0e3e
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-21 21:55:07 +0200

    switch function added

--- Task17/Task17/Program.cs
+++ Task17/Task17/Program.cs
@@ -11,10 +11,21 @@ namespace Task17
              * The program only performs the indicated operation. Please use the switch statement*/
 
             double a, b, sign;
-            Console.WriteLine("Enter FIRST number: ");
+            Console.Write("Enter FIRST number: ");
             a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter SECOND number: ");
+            Console.Write("Enter SECOND number: ");
             a = double.Parse(Console.ReadLine());
+
+            Console.Write("\nExpression symbol: ");
+            sign = double.Parse(Console.ReadLine());
+            switch (sign)
+            {
+                case '+': Console.WriteLine("+"); break;
+                case '-': Console.WriteLine("-"); break;
+                case '*': Console.WriteLine("*"); break;
+                case '/': Console.WriteLine("/"); break;
+                default: Console.WriteLine("Option out of range!"); break;
+            }
         }
     }
 }
