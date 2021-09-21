e6445d7e282b2f925060ebc1c63a5c502797a4c2
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-21 16:33:42 +0200

    description changed

---
+++ Task17/Task17/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that reads two numbers, the sign of an action, and on that basis did a computation. 
+             * The action sign is to be entered with one of the signs + - * / 
+             * The program only performs the indicated operation. Please use the switch statement*/
+
+
+        }
+    }
+}
