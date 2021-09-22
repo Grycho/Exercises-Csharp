f19303f5d075b19f50f8aa2a85709d19e9d1fae9
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 21:30:52 +0200

    description added

---
+++ Task21/Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that loads numbers for as long as the number is in the range -100 to 100. 
+             * Calculates the sum the numbers loaded.*/
+
+
+        }
+    }
+}
