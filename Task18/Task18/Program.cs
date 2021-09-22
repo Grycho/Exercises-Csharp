da43f180febbc744ba86a91b8f965f1dd385b596
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 14:41:12 +0200

    description added

---
+++ Task18/Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program to convert Anglo-Saxon units of measurement into Polish. 
+             *Use the switch selection statement. The program is supposed to perform 
+             *the following changes of measures: length units, mass units, volume units*/
+
+
+        }
+    }
+}
