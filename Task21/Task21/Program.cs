9bbff3b4ac4cc9f9d9994a781593aa583f6af4ff
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 21:43:39 +0200

    do while loops added

--- Task21/Task21/Program.cs
+++ Task21/Task21/Program.cs
@@ -9,7 +9,15 @@ namespace Task21
             /*A program that loads numbers for as long as the number is in the range -100 to 100. 
              * Calculates the sum the numbers loaded.*/
 
+            int num;
 
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("You entered a number: {0}", num);
+            } 
+            while (num >= -100 && num <= 100);
+            Console.WriteLine("\nYou entered a value out of range - you finished");
         }
     }
 }
