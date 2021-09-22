aae7e34b850550ee55d19070632529ab2fad0535
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 15:45:26 +0200

    another switch statement added

--- Task18/Task18/Program.cs
+++ Task18/Task18/Program.cs
@@ -10,17 +10,38 @@ namespace Task18
              *Use the switch selection statement. The program is supposed to perform 
              *the following changes of measures: length units, mass units, volume units*/
 
-            int length, mass, volume;
-            Console.WriteLine("Which unit do you want to calculate? Select the appropriate letter for the selected unit\n");
-
+            Console.WriteLine("Which unit do you want to calculate?");
+            Console.WriteLine("1. Unit of LENGTH");
+            Console.WriteLine("2. Unit of MASS");
+            Console.WriteLine("3. Unit of VOLUME");
+            Console.Write("\nSelect: ");
             int menu;
             menu = int.Parse(Console.ReadLine());
             switch (menu)
             {
-                case 1: Console.WriteLine("1. Unit of LENGTH: "); break;
-                case 2: Console.WriteLine("2. Unit of MASS: "); break;
-                case 3: Console.WriteLine("3. Unit of VOLUME: "); break;
-                default: Console.WriteLine("Selected option out of range!"); break;
+                case 1: 
+                    Console.WriteLine("1. Miles -> Kilometers"); 
+                    Console.WriteLine("2. Jards -> Meters"); 
+                    Console.WriteLine("3. Foots -> Meters"); 
+                    Console.WriteLine("4. Inches -> Centimeters");
+                    double length;
+                    length = double.Parse(Console.ReadLine());
+                    switch (length)
+                    {
+                        case 1:  ;break;
+                        default:  ;break;
+                    }
+                    ; break;
+                case 2: 
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    break;
+                case 3: 
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    break;
+                default: Console.WriteLine("\nSelected option out of range!"); break;
             }
         }
     }
