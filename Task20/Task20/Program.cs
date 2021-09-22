14595142da8b05932f5e2a7ecfeba88c843e6a9f
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 21:21:33 +0200

    loop for and description added

---
+++ Task20/Task20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that will display the numbers from 0 to 1000
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
