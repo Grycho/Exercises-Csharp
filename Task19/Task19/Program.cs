847f0943f9811190af1af0fa6fe7e974c07ee8aa
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-22 20:05:34 +0200

    description and variables added

---
+++ Task19/Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The price of a Bus ticket depends on the length of the route according to the following scheme: 
+             *for journeys from 0km to 10km you pay 2$, for journeys from 11 to 30km the rate is 1$ plus 0.10$ 
+             *for each kilometer, and for journeys over 30 km the rate is 1$ plus 0.08$ for every kilometer.*/
+
+            int km;
+            double ticket;
+        }
+    }
+}
