1f5ebea0b28fab109bae64e648bddf3df7cac0a0
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 19:19:23 +0200

    description and variables added

---
+++ Task22/Task22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read two numbers. Display more from less to greater.
+
+            int a, b, c;
+        }
+    }
+}
