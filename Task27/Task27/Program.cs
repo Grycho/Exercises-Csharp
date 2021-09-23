d56d8b8c79c7b3f88e2b2b25b5b76643fe5c9f29
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 22:24:26 +0200

    loop and if function added

--- Task27/Task27/Program.cs
+++ Task27/Task27/Program.cs
@@ -9,11 +9,18 @@ namespace Task27
             /*Program loads the numbers until 0 is entered. 
              *The program returns the number of numbers greater than 15*/
 
-            int num;
-            Console.Write("Input a number: ");
-            num = int.Parse(Console.ReadLine());
+            int num, sum=0;
 
-
+            do
+            {
+                Console.Write("Input a number: ");
+                num = int.Parse(Console.ReadLine());
+                if (num > 15)
+                {
+                    sum++;
+                }
+            } while (num != 0);
+            Console.WriteLine("\nYou entered '0' program is end - Numbers greater than 15 was: {0}", sum);
         }
     }
 }
