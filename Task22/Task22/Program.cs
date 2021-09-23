88a4ceae35ce010e819e36223b5c7de463927364
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 19:25:32 +0200

    if function added

--- Task22/Task22/Program.cs
+++ Task22/Task22/Program.cs
@@ -6,9 +6,20 @@ namespace Task22
     {
         static void Main(string[] args)
         {
-            //Read two numbers. Display more from less to greater.
+            //Read two numbers. Display from less to greater.
 
             int a, b, c;
+            Console.Write("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a < b)
+            {
+                c = a;
+                a = b;
+                b = c;
+            }
         }
     }
 }
