6f3c843462480db9de273192a653b451aa6b04c9
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 21:29:37 +0200

    more variables and loop for added

--- Task26/Task26/Program.cs
+++ Task26/Task26/Program.cs
@@ -8,7 +8,24 @@ namespace Task26
         {
             //A program that draws a "Christmas tree" composed of asterisks (*) in the console.
 
+            int num;
+            Console.WriteLine("How high should the christmas tree be?");
+            Console.Write("Input how many lines: ");
+            num = int.Parse(Console.ReadLine());
 
+            int manySpaces = num - 1, manyStars = 1;
+            for (int line = 1; line <= num; line++)
+            {
+                for (int space = 0; space < manySpaces; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 0; star < manyStars; star++)
+                {
+                    Console.Write("*");
+                }
+
+            }
         }
     }
 }
