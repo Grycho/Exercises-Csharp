8825d8244a4eead02a688eea9dbf1d44bc342702
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 19:45:28 +0200

    description added

---
+++ Task23/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculating the Greatest Common Divisor.
+
+
+        }
+    }
+}
