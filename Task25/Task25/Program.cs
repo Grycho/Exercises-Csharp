c7d061f8af4290040a2d3139aece90b0c74fb788
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 20:47:20 +0200

    description added

---
+++ Task25/Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program of testing whether the given number is Prime number. 
+             *Minimize the number of operations dividing*/
+
+
+        }
+    }
+}
