4dcb39c98633a80e2dbdd77f34bc9d0783dcb784
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 21:47:32 +0200

    variable added

--- Task27/Task27/Program.cs
+++ Task27/Task27/Program.cs
@@ -9,6 +9,10 @@ namespace Task27
             /*Program loads the numbers until 0 is entered. 
              *The program returns the number of numbers greater than 15*/
 
+            int num;
+            Console.Write("Input a number: ");
+            num = int.Parse(Console.ReadLine());
+
 
         }
     }
