31544c6e047a829468ec17570f5bf4be67be181b
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 21:44:54 +0200

    description added

---
+++ Task27/Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program loads the numbers until 0 is entered. 
+             *The program returns the number of numbers greater than 15*/
+
+
+        }
+    }
+}
