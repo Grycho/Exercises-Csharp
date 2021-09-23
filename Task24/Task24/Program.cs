b224a0725b361a7276c1eeeda1ad543ea8e8962c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 20:26:06 +0200

    description added

---
+++ Task24/Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program for calculating the sum of the digits 
+             *of the decimal expansion for a given natural number.*/
+
+            
+        }
+    }
+}
