3974b59a8a7149012efd670ddec8264534c2ff5c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-23 21:13:29 +0200

    description added

---
+++ Task26/Task26/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A program that draws a "Christmas tree" composed of asterisks (*) in the console.
+
+
+        }
+    }
+}
