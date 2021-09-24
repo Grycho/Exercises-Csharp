07c13bda31b397c587fa9ece665d872da7431baa
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 15:19:23 +0200

    loops and table added

--- Task28/Task28/Program.cs
+++ Task28/Task28/Program.cs
@@ -8,7 +8,18 @@ namespace Task28
         {
             //Displaying even numbers from the keyboard.
 
-
+            int[] tab = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if(tab[i] % 2 == 0)
+                {
+                    Console.WriteLine("\nEven numbers: " + tab[i]);
+                }
+            }
         }
     }
 }
