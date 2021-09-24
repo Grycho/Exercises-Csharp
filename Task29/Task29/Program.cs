08e172155c4101c3fb52eeee126eac1ae9604fba
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 15:34:23 +0200

    description added

---
+++ Task29/Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sum of values in cells. Required auxiliary variable for calculating sum
+
+
+        }
+    }
+}
