cb1661b8b377fed22084be4a6ff21a85a80b7576
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 15:52:22 +0200

    description added

---
+++ Task30/Task30/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program counts the sum of 1000 random values.
+
+
+        }
+    }
+}
