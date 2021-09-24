789c7eff4d101a50db98e01f94e3c0cc44bec6df
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 16:10:17 +0200

    foreach added

--- Task30/Task30/Program.cs
+++ Task30/Task30/Program.cs
@@ -11,7 +11,16 @@ namespace Task30
             Random draws = new Random();
             int[] tab = new int[1000];
 
-
+            for (int i = 0; i < 1000; i++)
+            {
+                tab[i] = draws.Next(10, 50);
+            }
+            int sum = 0;
+            foreach (var element in tab)
+            {
+                sum += element;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
