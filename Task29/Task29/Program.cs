a0a26e427e5eca9aee8b56a53cbf63c430ab565c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 15:38:19 +0200

    loop foreach and for added + tab

--- Task29/Task29/Program.cs
+++ Task29/Task29/Program.cs
@@ -8,7 +8,17 @@ namespace Task29
         {
             //Sum of values in cells. Required auxiliary variable for calculating sum
 
-
+            int[] tab = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (var element in tab)
+            {
+                sum += element;
+            }
+            Console.WriteLine("Sum of values: "+ sum);
         }
     }
 }
