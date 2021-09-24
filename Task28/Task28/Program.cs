703b64e68770c85238597f7d11e41b92a771ad81
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-24 15:12:19 +0200

    description added

---
+++ Task28/Task28/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying even numbers from the keyboard.
+
+
+        }
+    }
+}
