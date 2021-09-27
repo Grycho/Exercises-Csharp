4633cfa2348a101156e5fffc8ad8644be95cd215
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 19:06:27 +0200

    array and for loop added

--- Task32/Task32/Program.cs
+++ Task32/Task32/Program.cs
@@ -8,7 +8,13 @@ namespace Task32
         {
             //Program draws 1000 numbers from the range (1,10000) and displays the numbers above the average.
 
+            Random Numb = new Random();
+            int[] tab = new int[1000];
 
+            for (int i = 0; i < length; i++)
+            {
+
+            }
         }
     }
 }
