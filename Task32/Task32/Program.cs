378810ffa0cdbbb624c5a1526926f732ec067ac7
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 19:05:45 +0200

    description added

---
+++ Task32/Task32/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program draws 1000 numbers from the range (1,10000) and displays the numbers above the average.
+
+
+        }
+    }
+}
