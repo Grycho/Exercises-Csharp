04bb6af36b8dc239c890cdb5fe0ee4eeb50be2ba
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 21:22:03 +0200

    description added

---
+++ Task34/Task34/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program: Array of 1000 numbers <-100,1000>. Every value <100 has 10,000 added.
+
+
+        }
+    }
+}
