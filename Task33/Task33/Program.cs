e331e5c04c58b474d81bbaaa2835219c17fa56f1
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 19:43:08 +0200

    for and foreach loop added

--- Task33/Task33/Program.cs
+++ Task33/Task33/Program.cs
@@ -8,7 +8,18 @@ namespace Task33
         {
             //Program that determines how many times the maximum.
 
+            Random Numb = new();
+            int[] tab = new int[1000];
 
+            for (int i = 0; i < 1000; i++)
+            {
+                tab[i] = Numb.Next(-100, 30000);
+            }
+            int Max = tab[0];
+            foreach (var Element in tab)
+            {
+
+            }
         }
     }
 }
