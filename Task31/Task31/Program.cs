2947d582a82ecf0fd8c179a3e9b5b69368cddc50
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 18:45:04 +0200

    description added

---
+++ Task31/Task31/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determining the maximum in an array of random numbers
+
+
+        }
+    }
+}
