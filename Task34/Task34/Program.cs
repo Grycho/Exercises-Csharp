64a932fa4d181b4da03c4cd00cd06807262d04d1
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-27 21:24:07 +0200

    array and loop for added

--- Task34/Task34/Program.cs
+++ Task34/Task34/Program.cs
@@ -8,7 +8,13 @@ namespace Task34
         {
             //Program: Array of 1000 numbers <-100,1000>. Every value <100 has 10,000 added.
 
+            Random Numb = new();
+            int[] tab = new int[1000];
 
+            for (int i = 0; i < 1000; i++)
+            {
+                tab[i] = Numb.Next(-100, 1000);
+            }
         }
     }
 }
