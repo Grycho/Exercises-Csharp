01bc7e43bc5bba2ced2ba7418f03e276de596d63
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-28 19:30:26 +0200

    for loops and array added

--- Task35/Task35/Program.cs
+++ Task35/Task35/Program.cs
@@ -8,7 +8,21 @@ namespace Task35
         {
             //Program draws 30 numbers <0,100>. First it displays even cells, later odd numbers
 
+            Random Numb = new();
+            int[] tab = new int[30];
 
+            for (int i = 0; i < 30; i++)
+            {
+                tab[i] = Numb.Next(0, 100);
+            }
+            for (int i = 0; i < 30; i+=2)
+            {
+                Console.WriteLine();
+            }
+            for (int i = 1; i < 30; i+=2)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
