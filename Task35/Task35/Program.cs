8329e93a2b1f1a15fb65e1e40147b89a3182f0ca
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-28 19:26:46 +0200

    description added

---
+++ Task35/Task35/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program draws 30 numbers <0,100>. First it displays even cells, later odd numbers
+
+
+        }
+    }
+}
