f7cddeeea339806f0784aa95ca3e2167ef21d79a
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-28 19:44:19 +0200

    variables and random draw added

---
+++ Task36/Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program is to draw 1000 numbers <-20000,20000> to the array to calculate 
+             *the span of the set (difference between the maximum and minimum).*/
+
+            Random Numb = new();
+            int[] tab = new int[1000];
+
+
+        }
+    }
+}
