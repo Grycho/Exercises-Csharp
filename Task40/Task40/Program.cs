d9491d59db574762c9f751d410713a98c1a964a1
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-29 14:27:51 +0200

    for loop added

---
+++ Task40/Task40/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Words = new string[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Words[i] = Console.ReadLine();
+            }
+
+        }
+    }
+}
