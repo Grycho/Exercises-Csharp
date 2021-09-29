1d9037c7bc696c6f124075d501708908de692651
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-29 13:39:42 +0200

    description added

---
+++ Task37/Task37/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program prints the loaded string from the end
+
+
+        }
+    }
+}
