2730c67118396916b289f69ffe2f238491b368c7
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-29 14:09:08 +0200

    variables and description added

---
+++ Task39/Task39/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check if the string is a palindrome
+
+            string Word, Palindrome="";
+            Word = Console.ReadLine();
+        }
+    }
+}
