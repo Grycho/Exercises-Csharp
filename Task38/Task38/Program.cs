e20525ca0b20d494e387821f4782af20ccb82faf
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-29 13:57:12 +0200

    description and variables added

---
+++ Task38/Task38/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program remembers the inverted inscription
+
+            string Word, Remembered = "";
+
+        }
+    }
+}
