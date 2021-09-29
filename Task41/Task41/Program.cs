452ebfc6d3037f9573166fd4199b4ed1d70aaf45
Szymon <szymon.gryszkiewicz@gmail.com>
2021-09-29 16:35:57 +0200

    description and structure added

---
+++ Task41/Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task41
+{
+    class Program
+    {
+        struct DataHuman
+        {
+            public string Name, Surname;
+            public int Age, Height;
+        }
+        static void Main(string[] args)
+        {
+            /*[Structures]The program will read data about any object, each object described with 5 parameters. 
+             *The program can enter descriptions of 4 objects and displays them from the end*/
+
+
+        }
+    }
+}
