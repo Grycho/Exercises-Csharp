29409a037f61bc1fe103d0b66c20a49b88f9b290
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 22:20:23 +0200

    variables added [under program]

--- Task45/Task45/Program.cs
+++ Task45/Task45/Program.cs
@@ -9,7 +9,10 @@ namespace Task45
             /*The program includes a function that loads two coordinates of the segment 
              *x1, y1, x2, y2 and returns the length of the segment (non-integer)*/
 
-            
+            double LengthSection(int x1, int y1, int x2, int y2)
+            {
+
+            }
         }
     }
 }
