ba4cfa73b29d7032f334aad3eebaaee53914f403
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 21:03:21 +0200

    for loops and array added

--- Task42/Task42/Program.cs
+++ Task42/Task42/Program.cs
@@ -8,7 +8,15 @@ namespace Task42
         {
             //Draw a 10x10 matrix and display an array, then display the sum of each column
 
+            Random Numb = new();
+            int[,] tab = new int[10, 10];
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
 
+                }
+            }
         }
     }
 }
