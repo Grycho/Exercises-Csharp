2ed723b8f0b23184d968acfb0f5d224657fff304
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 20:14:41 +0200

    description added

---
+++ Task42/Task42/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Draw a 10x10 matrix and display an array, then display the sum of each column
+
+
+        }
+    }
+}
