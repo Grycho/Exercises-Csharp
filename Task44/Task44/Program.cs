495b108f18b7e2f24b6852f95eaeb82b2e6fe2b7
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 21:53:16 +0200

    variables added

--- Task44/Task44/Program.cs
+++ Task44/Task44/Program.cs
@@ -8,6 +8,10 @@ namespace Task44
         {
             //Program error catching
 
+            int Numb1, Numb2;
+            bool Error = false;
+            double Sum;
+            bool Quant1, Quant2;
 
         }
     }
