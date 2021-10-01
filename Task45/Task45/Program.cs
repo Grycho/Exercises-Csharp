040ef4055b1fde9cfec8f865a29d75c5cd2f3508
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 22:33:03 +0200

    Test: everything works

--- Task45/Task45/Program.cs
+++ Task45/Task45/Program.cs
@@ -9,7 +9,7 @@ namespace Task45
             /*The program includes a function that loads two coordinates of the segment 
              *x1, y1, x2, y2 and returns the length of the segment (non-integer)*/
 
-            double LengthSection(int x1, int y1, int x2, int y2)
+            static double LengthSection(int x1, int y1, int x2, int y2)
             {
                 double Value;
                 Value = Math.Abs(x2 - x2) * Math.Abs(x2 - x1) + Math.Abs(y1 - y2) * Math.Abs(y1 - y2);
@@ -17,15 +17,15 @@ namespace Task45
                 return Value;
             }
             int Numb1, Numb2, Numb3, Numb4;
-            Console.WriteLine("First coordinate: ");
+            Console.Write("First coordinate: ");
             Numb1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("First coordinate: ");
+            Console.Write("Second: ");
             Numb2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("First coordinate: ");
+            Console.Write("Third: ");
             Numb3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("First coordinate: ");
+            Console.Write("Fourth: ");
             Numb4 = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nResult: {0}", LengthSection(Numb1, Numb2, Numb3, Numb4));
+            Console.WriteLine("\nSection length is: {0}", LengthSection(Numb1, Numb2, Numb3, Numb4));
         }
     }
 }
