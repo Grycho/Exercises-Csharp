ee9ec52455ac1a2764a5489d31c1899c4af15875
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 22:19:08 +0200

    description added

---
+++ Task45/Task45/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program includes a function that loads two coordinates of the segment 
+             *x1, y1, x2, y2 and returns the length of the segment (non-integer)*/
+
+            
+        }
+    }
+}
