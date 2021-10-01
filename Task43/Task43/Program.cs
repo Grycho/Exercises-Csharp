52e9222b61513fccf5113186f9b858de352afc6c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-01 21:13:05 +0200

    random function and array added

---
+++ Task43/Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Draw a 15x15 matrix, display, give the maximum value and 
+             *coordinates of the cell with the maximum value.*/
+
+            Random Numb = new();
+            int[] tab = new int[10];
+
+
+        }
+    }
+}
