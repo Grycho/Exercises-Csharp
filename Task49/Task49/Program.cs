1c2ebe781945700888ee44ab584c95ba34a5f522
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 19:33:16 +0200

    description added

---
+++ Task49/Task49/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a field function that computes a trapezoid and displays it
+
+
+        }
+    }
+}
