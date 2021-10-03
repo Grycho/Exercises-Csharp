7f17d8a923892e4b709c562ab6c6e45b7b42f899
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 19:43:17 +0200

    Test: everything works

--- Task49/Task49/Program.cs
+++ Task49/Task49/Program.cs
@@ -8,15 +8,18 @@ namespace Task49
         {
             //Write a field function that computes a trapezoid and displays it
 
-            void Area(int Length, int Width, int Height)
+            static void Area(int Length, int Width, int Height)
             {
                 double AreaResult;
                 AreaResult = 0.5 * (Length + Width) * Height;
                 Console.WriteLine("\nArea of the trapezoid is: " + AreaResult);
             }
             int Numb1, Numb2, Numb3;
+            Console.Write("Enter the length: ");
             Numb1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the width: ");
             Numb2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
             Numb3 = int.Parse(Console.ReadLine());
             Area(Numb1, Numb2, Numb3);
         }
