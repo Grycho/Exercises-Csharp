34768986b8f915c695de19e2f019c1784eff84c6
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 18:56:04 +0200

    Test: everything works

--- Task47/Task47/Program.cs
+++ Task47/Task47/Program.cs
@@ -14,20 +14,20 @@ namespace Task47
              * into the smallest possible amount of coins and banknotes with 
              * the denominations of 1, 2, 5, 10 PLN*/
 
-            Coins Banknotes(int x)
+            static Coins Banknotes(int x)
             {
                 Coins Value;
                 Value.Bankn10 = x / 10; x %= 10;
-                Value.Coin5 = x / 10; x %= 5;
+                Value.Coin5 = x / 5; x %= 5;
                 Value.Coin2 = x / 2; x %= 2;
                 Value.Coin1 = x / 1;
                 return Value;
             }
             int Numb;
-            Console.WriteLine("Enter the amount: ");
+            Console.Write("Enter the amount: ");
             Numb = int.Parse(Console.ReadLine());
             Coins c = Banknotes(Numb);
-            Console.WriteLine("\nAmount of coins: 1PLN={0}, 2PLN={1}, 5PLN={2} and banknotes: 10PLN={3}", c.Coin1, c.Coin2, c.Coin5, c.Bankn10);
+            Console.WriteLine("\nAmount of coins: 1PLN = {0}, 2PLN = {1}, 5PLN = {2}, banknotes: 10PLN = {3}", c.Coin1, c.Coin2, c.Coin5, c.Bankn10);
         }
     }
 }
