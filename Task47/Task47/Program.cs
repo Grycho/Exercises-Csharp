1332239de2440a06afeada3d197e7ea0202f4c9d
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 18:37:48 +0200

    structure added

--- Task47/Task47/Program.cs
+++ Task47/Task47/Program.cs
@@ -4,12 +4,15 @@ namespace Task47
 {
     class Program
     {
+        struct Coin
+        {
+            public int Coin1, Coin2, Coin5, Bankn10;
+        }
         static void Main(string[] args)
         {
             /*Program that converts the amount of money (integer) specified by the user 
              * into the smallest possible amount of coins and banknotes with 
              * the denominations of 1, 2, 5, 10 PLN*/
-
 
         }
     }
