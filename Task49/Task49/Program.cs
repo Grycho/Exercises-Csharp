dcb53fc72b599ec5b5533c1f07adbeac8f8229b8
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 19:36:57 +0200

    variables added

--- Task49/Task49/Program.cs
+++ Task49/Task49/Program.cs
@@ -8,7 +8,12 @@ namespace Task49
         {
             //Write a field function that computes a trapezoid and displays it
 
-
+            void Area(int Length, int Width, int Height)
+            {
+                double AreaResult;
+                AreaResult = 0.5 * (Length + Width) * Height;
+                Console.WriteLine("\nArea of the trapezoid is: ");
+            }
         }
     }
 }
