7f7cd3b194d3787a5ef5530f18c53f28715997fe
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 18:12:55 +0200

    description added

---
+++ Task46/Task46/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a NumberDigits function that returns the number of digits in an integer
+
+
+        }
+    }
+}
