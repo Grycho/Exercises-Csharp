145c9544ea6301ae6f5097e1f8d59da6d49a6958
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 18:23:04 +0200

    variables and while loop added

--- Task46/Task46/Program.cs
+++ Task46/Task46/Program.cs
@@ -8,7 +8,16 @@ namespace Task46
         {
             //Write a NumberDigits function that returns the number of digits in an integer
 
-
+            int NumberDigits(int x)
+            {
+                int Value = 0;
+                while (x!= 0)
+                {
+                    Value++;
+                    x /= 10;
+                }
+                return Value;
+            }
         }
     }
 }
