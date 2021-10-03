b32d50368658143108ec29de79fd9f857fc64753
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 18:35:45 +0200

    description added

---
+++ Task47/Task47/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program that converts the amount of money (integer) specified by the user 
+             * into the smallest possible amount of coins and banknotes with 
+             * the denominations of 1, 2, 5, 10 PLN*/
+
+
+        }
+    }
+}
