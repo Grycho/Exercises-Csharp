2d97942d8845b97dd468c62b9ccef8cbc814a171
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-03 19:05:35 +0200

    description added

---
+++ Task48/Task48/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that takes a positive natural number N (n < 30) from the user 
+             * and creates an array 'a' of boolean variables of size n × n.
+             * 
+             * Then the program should fill the created array, so that a [i] [j] = true, 
+             * if the numbers (i + 1) and (j + 1) are relatively prime, i.e. they do not 
+             * have common divisors beyond 1. The array created in this way should be printed 
+             * on the screen, the "+" sign should be displayed for the true value, 
+             * and the sign for the false value is "." */
+
+
+        }
+    }
+}
