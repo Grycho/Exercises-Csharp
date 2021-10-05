00513cd3c68f71b5bd24b9f9112d78b5ff105de7
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-05 21:11:06 +0200

    description added

---
+++ Task50/Task50/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that asks the user to enter the length of a string of numbers, 
+             * and then reads those numbers into the array one by one. Then the program 
+             * transfers the data of the function, which is to calculate the average of 
+             * the given numbers and write it to the screen as follows: 
+             * The average of the numbers 1 2 3 4 5 6 is 3.5 */
+             //Program must be protected against division by 0 and entering incorrect values
+
+
+        }
+    }
+}
