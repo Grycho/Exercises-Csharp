b43a3b3932bc8542fdbbc6bfac6fccde09f796fb
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 21:52:21 +0200

    description added

---
+++ Task53/Task53/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A program that draws a Christmas tree from stars with a value specified by the user. 
+             *Example for value 4 (height 4 is for the lower element, higher is 1 less)*/
+
+
+        }
+    }
+}
