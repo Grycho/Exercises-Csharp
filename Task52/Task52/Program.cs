846c6111e150d8419b8146ddba9471d9c09fe46e
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 19:31:06 +0200

    description added

---
+++ Task52/Task52/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Function that draws a horizontal histogram from asterisks, illustrating 
+             *the distribution of values in a user-supplied string of numbers between 1-5.*/
+
+
+        }
+    }
+}
