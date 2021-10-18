87f89e6c0ea6dbd400e6cb7a9bb623d43a0d4f69
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 22:22:14 +0200

    variable and appeal to method added

--- Task53/Task53/Program.cs
+++ Task53/Task53/Program.cs
@@ -9,11 +9,11 @@ namespace Task53
             /*A program that draws a Christmas tree from stars with a value specified by the user. 
              *Example for value 4 (height 4 is for the lower element, higher is 1 less)*/
 
-            static void Tree (int Numb)
+            static void Tree (int Star)
             {
-                for (int i = 0; i < Numb - 1; i++)
+                for (int i = 0; i < Star - 1; i++)
                 {
-                    for (int j = 0; j < Numb - 1 - i; j++)
+                    for (int j = 0; j < Star - 1 - i; j++)
                         Console.Write(" ");
                     for (int j = 1; j <= 1 + i * 2; j++)
                     {
@@ -21,14 +21,21 @@ namespace Task53
                     }
                     Console.WriteLine();
                 }
-                for (int i = 0; i < Numb; i++)
+                for (int i = 0; i < Star; i++)
                 {
-                    for (int j = 0; j < Numb - 1 - i; j++)
+                    for (int j = 0; j < Star - 1 - i; j++)
+                        Console.Write(" ");
+                    for (int j = 1; j <= 1 + i * 2; j++)
                     {
-
+                        Console.Write("*");
                     }
+                    Console.WriteLine();
                 }
             }
+            Console.Write("How many layers should the Christmas tree have: ");
+            int Numb = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            Tree(Numb);
         }
     }
 }
