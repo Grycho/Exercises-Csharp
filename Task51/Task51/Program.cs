d303d25ec10bbc260da75626b5de8aa6a014d66c
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 18:15:52 +0200

    static method and for loop added

--- Task51/Task51/Program.cs
+++ Task51/Task51/Program.cs
@@ -4,6 +4,13 @@ namespace Task51
 {
     class Program
     {
+        static bool Prime(int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+            {
+
+            }
+        }
         static void Main(string[] args)
         {
             //A function that checks whether the given number is prime
