edcea25d71dd1a7740eb3c8ed0e6d2c2b5335cc1
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 18:16:43 +0200

    if loop with returns added

--- Task51/Task51/Program.cs
+++ Task51/Task51/Program.cs
@@ -8,8 +8,9 @@ namespace Task51
         {
             for (int i = 2; i * i <= n; i++)
             {
-
+                if (n % i == 0) return false;
             }
+            return true;
         }
         static void Main(string[] args)
         {
