6269a24b9e214bf0ef47d925aad5515f6522f4da
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 19:47:41 +0200

    static method,array, foreach loop and counter added

--- Task52/Task52/Program.cs
+++ Task52/Task52/Program.cs
@@ -9,7 +9,14 @@ namespace Task52
             /*Function that draws a horizontal histogram from asterisks, illustrating 
              *the distribution of values in a user-supplied string of numbers between 1-5.*/
 
-
+            static void Chart(params int [] Tab)
+            {
+                int[] Count = new int[6];
+                foreach (var Element in Tab)
+                {
+                    Count[Element - 1]++;
+                }
+            }
         }
     }
 }
