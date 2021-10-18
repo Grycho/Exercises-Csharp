7834c8b81af58c7ab472f26fdc3c0c2f874f043a
Szymon <szymon.gryszkiewicz@gmail.com>
2021-10-18 18:09:09 +0200

    description added

---
+++ Task51/Task51/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A function that checks whether the given number is prime
+
+
+        }
+    }
+}
